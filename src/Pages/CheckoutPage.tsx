26a267c3c05d6fb5785565a327d484bb3befb7a6
Ubaid ur Rehman <mr.ubaid92222@gmail.com>
2026-01-08 18:25:06 +0500

    Pages Routing

--- src/Pages/CheckoutPage.tsx
+++ src/Pages/CheckoutPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './checkout.css';
 import './checkout-header.css';
+import { Link } from 'react-router';
 
 export default function CheckoutPage() {
   return (
@@ -8,15 +9,15 @@ export default function CheckoutPage() {
          <div className="checkout-header">
       <div className="header-content">
         <div className="checkout-header-left-section">
-          <a href="/">
+          <Link to="/">
             <img className="logo" src="images/logo.png" />
             <img className="mobile-logo" src="images/mobile-logo.png" />
-          </a>
+          </Link>
         </div>
 
         <div className="checkout-header-middle-section">
-          Checkout (<a className="return-to-home-link"
-            href="/">3 items</a>)
+          Checkout (<Link className="return-to-home-link"
+            to="/">3 items</Link>)
         </div>
 
         <div className="checkout-header-right-section">
